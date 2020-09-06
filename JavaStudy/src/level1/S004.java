b107811287f616ad607ec8b25de18eb05608ffe3
jinyoung1704 <jinyoung1704@naver.com>
2020-09-06 21:37:27 +0900

    풀이 추가

--- JavaStudy/src/level1/S004.java
+++ JavaStudy/src/level1/S004.java
@@ -28,3 +28,22 @@ public class S004
 	}
 
 }
+
+/* 다른사람 풀이
+ class SumMatrix {
+ 
+    int[][] sumMatrix(int[][] A, int[][] B) {
+    int row = Math.max(A.length, B.length);
+    int col = Math.max(A[0].length, B[0].length);
+        //int[][] answer = {{0, 0}, {0, 0}};
+    int[][] answer = new int[row][col];
+    for(int i=0; i<row ; i++){
+      for(int j=0; j<col; j++){
+        answer[i][j] = A[i][j] + B[i][j];
+      }
+    }
+
+        return answer;
+    }
+ */
+ 
