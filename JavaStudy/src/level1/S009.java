2bcd52cea10aa66c996fae77ce6519428df6bfb9
jinyoung1704 <jinyoung1704@naver.com>
2020-09-09 23:33:17 +0900

    문자열 내 마음대로 정렬하기

---
+++ JavaStudy/src/level1/S009.java
@@ -0,0 +1,54 @@
+package level1;
+/*
+ 	문자열 내 마음대로 정렬하기 
+ 	
+ 	문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 
+ 	예를 들어 strings가 [sun, bed, car]이고 n이 1이면 각 단어의 인덱스 1의 문자 u, e, a로 strings를 정렬합니다.
+ 
+*/
+
+import java.util.Arrays;
+
+public class S009
+{
+	public static String[] main(String[] strings, int n)
+	{
+		 String[] answer = new String[strings.length];
+	        char[] arr = new char[strings.length];
+	        for(int i=0;i<strings.length;i++)
+	            arr[i] = strings[i].charAt(n);
+	        
+	        for(int i=0;i<arr.length;i++)
+	        {
+	            for(int j=0;j<arr.length-1-i;j++)
+	            {
+	                if(arr[j]>arr[j+1])
+	                {
+	                    char temp = arr[j];
+	                    arr[j] = arr[j+1];
+	                    arr[j+1] = temp;
+	                    
+	                    System.out.println(arr[0]);
+	                    System.out.println(arr[1]);
+	                    System.out.println(arr[2]);
+	                    
+	                    String word = strings[j];
+	                    strings[j] = strings[j+1];
+	                    strings[j+1] = word;
+	                }
+	                else if(arr[j]==arr[j+1])
+	                    Arrays.sort(strings);
+	                
+	            }
+	        }
+	        
+	        for(int i=0;i<strings.length;i++)
+	            answer[i] = strings[i];
+	            
+	        
+	        
+	        
+	        return answer;
+	}
+
+}
