8c1316dbcbe62cc76bbfa3358a90bc21225c16da
jinyoung1704 <jinyoung1704@naver.com>
2020-09-25 23:10:19 +0900

    홀짝판별

---
+++ JavaStudy/src/goorm/G005.java
@@ -0,0 +1,18 @@
+package goorm;
+//홀짝 판별
+import java.io.*;
+
+public class G005
+{
+
+}
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int su = Integer.parseInt(input);
+			System.out.println(su%2==0 ? "even" : "odd");
+
+	}
+}
