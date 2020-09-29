c9d09e63ac1045ac9de56ebd9748a060dd1f7876
jinyoung1704 <jinyoung1704@naver.com>
2020-09-29 16:05:31 +0900

    구름 약수의 합

--- JavaStudy/src/goorm/G011.java
+++ JavaStudy/src/goorm/G011.java
@@ -1,6 +1,20 @@
 package goorm;
+//약수의 합
+import java.io.*;
 
-public class G011
-{
-
+class Main_g011 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for(int i=1;i<=n/2;i++)
+		{
+			if(n%i==0)
+				sum += i;
+		}
+		
+		sum += n;
+		
+		System.out.println(sum);
+	}
 }
