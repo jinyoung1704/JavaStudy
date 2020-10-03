81238f70cf4979c728c53f93a976d7b7cbefb829
jinyoung1704 <jinyoung1704@naver.com>
2020-10-03 23:07:05 +0900

    백준 15596 정수 N개의 합

---
+++ JavaStudy/src/baekjoon/b010.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class b010 {
+    long sum(int[] a) {
+    	
+        long ans = 0;
+		
+		for(int i=0;i<a.length;i++)
+			ans += a[i];
+        
+        return ans;
+    }
+}
