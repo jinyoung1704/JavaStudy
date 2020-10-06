2b679c790b4024c3827c6cbd8f6bcac0c6c3d60f
jinyoung1704 <jinyoung1704@naver.com>
2020-10-06 15:30:20 +0900

    백준 1260번 DFS와 BFS

---
+++ JavaStudy/src/baekjoon/b015.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class b015
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(i%2!=0)
+			{
+				for(int j=1;j<=n;i++)
+					System.out.print("*");		
+			}
+			else
+			{
+				for(int j=1;j<=n;i++)
+				{
+					if(j%2==0)
+						System.out.print(" ");
+					else
+						System.out.print("*");
+				}
+			}
+			
+			System.out.println();
+				
+		}
+	}
+}
