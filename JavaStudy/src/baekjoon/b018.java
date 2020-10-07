74e9109203398e2a4b03f21c93b8761b2eeefaad
jinyoung1704 <jinyoung1704@naver.com>
2020-10-07 17:44:35 +0900

    백준 1026번 보물

--- JavaStudy/src/baekjoon/b018.java
+++ JavaStudy/src/baekjoon/b018.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class b018
 {
@@ -11,21 +12,34 @@ public class b018
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
+		
 		int[] A = new int[n];
 		int[] B = new int[n];
-	
-		String[] str;
+		
+		String[] str1 = br.readLine().split(" ");
+		String[] str2 = br.readLine().split(" ");
 		
 		for(int i=0;i<n;i++)
 		{
-			str = br.readLine().split(" ");
-			A[i] = Integer.parseInt(str[i]);
+			A[i] = Integer.parseInt(str1[i]);
 		}
+
+		for(int i=0;i<n;i++)
+		{
+			
+			B[i] = Integer.parseInt(str2[i]);
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int sum = 0;
 		
 		for(int i=0;i<n;i++)
 		{
-			str = br.readLine().split(" ");
-			B[i] = Integer.parseInt(str[i]);
+			sum += A[i] * B[n-1-i];
 		}
+		
+		System.out.println(sum);
 	}
 }	
