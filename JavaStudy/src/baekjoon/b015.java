7ae429f8340c3aa615db9cb4686b569697f75282
jinyoung1704 <jinyoung1704@naver.com>
2020-10-10 15:00:24 +0900

    백준 2606번 바이러스

--- JavaStudy/src/baekjoon/b015.java
+++ JavaStudy/src/baekjoon/b015.java
@@ -1,36 +1,62 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 //백준 별찍기 -10 
 
 public class b015
 {
+	static char[][] map = new char[6561][6561];
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		
-		for(int i=1;i<=n;i++)
+		sc.close();
+		
+		map = new char[n][n];
+	      
+	      for(int i=0;i<map.length;i++) {
+	         Arrays.fill(map[i],' ');
+	      }
+	      
+	      solve(n,0,0);
+	      
+	      for(int i=0;i<map.length;i++) {
+	         System.out.println(map[i]);
+	      }
+	   }
+
+		
+		
+		
+	
+	
+	static void solve(int n,int x,int y)
+	{
+		if(n==1)
 		{
-			if(i%2!=0)
-			{
-				for(int j=1;j<=n;i++)
-					System.out.print("*");		
-			}
-			else
-			{
-				for(int j=1;j<=n;i++)
-				{
-					if(j%2==0)
-						System.out.print(" ");
-					else
-						System.out.print("*");
-				}
-			}
-			
-			System.out.println();
-				
+			map[x][y] = '*';
+			return;
 		}
+			
+		int div = n/3;
+		
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(i==1 && j==1)
+					continue;
+				
+				solve(div, x + (div*i) , y + (div*j));
+			}
+		}
+		
 	}
 }
+
+//Arrays.fill(msg, 'a'); --==>> 배열의 값을 모두 a라는 문자로 초기화함
+
+
---
+++ JavaStudy/src/baekjoon/b025.java
@@ -0,0 +1,62 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class b025
+{
+	static int[][] map;
+	static int[] visit;
+	static int n ;
+	static int count;
+	static int computer;
+	public static void main(String[] args) throws NumberFormatException, IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		computer = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
+		String[] str;
+		
+		map = new int[computer+1][computer+1];
+		visit = new int[computer+1];
+		for(int i=1;i<=n;i++)
+		{
+			str = br.readLine().split(" ");
+			int a = Integer.parseInt(str[0]);
+			int b = Integer.parseInt(str[1]);
+			
+			map[a][b] = map[b][a] = 1;
+		}
+		
+		bfs(1);
+	}
+	
+	public static void bfs(int v)
+	{
+		Queue<Integer> qu = new LinkedList<Integer>();
+		qu.add(v);
+		visit[v]=1;
+		
+		while(!qu.isEmpty())
+		{
+			int temp = qu.poll();
+			
+			for(int i=1;i<=computer;i++)
+			{
+				if(map[temp][i]==1 && visit[i]!=1)
+				{
+					//System.out.println(i);
+					qu.add(i);
+					visit[i]=1;
+					count++;
+				}
+			}
+			
+		}
+		System.out.println(count);
+	}
+}
