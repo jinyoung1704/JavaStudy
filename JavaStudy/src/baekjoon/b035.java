e4d1ed3c4a6e42931cb4badc528dbec44e473bac
jinyoung1704 <jinyoung1704@naver.com>
2020-10-20 15:14:58 +0900

    백준 1259번 팰린드롬수

--- JavaStudy/src/baekjoon/b035.java
+++ JavaStudy/src/baekjoon/b035.java
@@ -1,14 +1,16 @@
+package baekjoon;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-public class Main
+// 백준 1259번 팰린드롬수
+
+public class b035
 {
-	
-	 public static void main(String[] args) throws NumberFormatException, IOException
-	   {
-	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	      int n;
 	      
@@ -29,7 +31,7 @@ public class Main
 	         
 	         if(n!=0)
 	         {
-	        	 if(str.equals(rStr))
+	        	 if(str.equals(rStr)) // ※ String 은 객체로 equals 써야 함!
 	 	            sb.append("yes");
 	 	         else
 	 	            sb.append("no");
@@ -42,13 +44,5 @@ public class Main
 	      
 	      
 	      System.out.println(sb.toString());
-	   }
+	}
 }
-
- 
-
-
-
-
-
-
