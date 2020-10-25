18f886b1f5be27310707b032cd5611bda8e791b6
jinyoung1704 <jinyoung1704@naver.com>
2020-10-25 15:32:56 +0900

    백준 유기농 배추

--- JavaStudy/src/Main.java
+++ JavaStudy/src/Main.java
@@ -6,19 +6,73 @@ import java.util.Stack;
 
 public class Main
 {
+	static int M; //가로
+	static int N; //세로
+	static int K;
+	static int[][] map,visit;
+	
+	static int[] nx = {0,0,1,-1};
+	static int[] ny = {1,-1,0,0};
+	static int count;
 	
 	 public static void main(String[] args) throws NumberFormatException, IOException
 	   {
 	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	      String word = br.readLine();
+	      int n = Integer.parseInt(br.readLine());
 	      
-	      System.out.println(word.substring(1,3));
-	      Stack<Character> s = new Stack<Character>();
-	      s.add('r');
-	      int index = word.indexOf("(");
-	      System.out.println(index);
+	      while(n-->0)
+	      {
+	    	  String[] temp = br.readLine().split(" ");
+	    	  M = Integer.parseInt(temp[0]);
+	    	  N = Integer.parseInt(temp[1]);
+	    	  K = Integer.parseInt(temp[2]);
+	    	  
+	    	  map = new int[N][M];
+	    	  visit = new int[N][M];
+	    	  count = 0;
+	    	  
+	    	  for(int i=1;i<=K;i++)
+	    	  {
+	    		  temp = br.readLine().split(" ");
+	    		  map[Integer.parseInt(temp[1])][Integer.parseInt(temp[0])] = 1;
+	    		  
+	    	  }
+	    	  
+	    	  for(int i=0;i<N;i++)
+	    	  {
+	    		  for(int j=0;j<M;j++)
+	    		  {
+	    			//System.out.print(map[i][j]);
+	    			  if(map[i][j]==1 && visit[i][j]==0)
+	    			 {
+	    				  dfs(i,j);
+	    				  count++;
+	    			 }
+	    			
+	    		  }//System.out.println();
+	    	  }
+	    	  
+	    	  System.out.println(count);
+	      }
 	   }
+	 
+	 public static void dfs(int x,int y)
+	 {
+		 visit[x][y] = 1;
+		 
+		 for(int i=0;i<4;i++)
+		 {
+			 int dx = x + nx[i];
+			 int dy = y + ny[i];
+			 
+			 if(dx>=0 && dy>=0 && dx<map.length && dy<map[0].length)
+			 {
+				 if(visit[dx][dy]==0 && map[dx][dy]==1)
+					 dfs(dx,dy);
+			 }
+		 }
+	 }
 }
 
  
