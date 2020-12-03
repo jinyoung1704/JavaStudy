19a908c82389bd6c9a761a80335a33689af9dc0d
jinyoung1704 <jinyoung1704@naver.com>
2020-12-03 23:57:08 +0900

    프로그래머스 제이든케이스

--- JavaStudy/src/level2/S021.java
+++ JavaStudy/src/level2/S021.java
@@ -8,42 +8,6 @@ public class S021
 class Solution_S021 {
     public String solution(String s) {
         String answer = "";
-        String[] str = s.split(" "); 
-
-       if(s.substring(s.length()-1).equals(" "))
-            str[str.length-1] += " ";
-
-        for(int i=0;i<str.length;i++)
-        {
-            for(int j=0;j<str[i].length();j++)
-            {
-                char cha = str[i].charAt(j);
-
-                if(j!=0 && cha>=65 && cha<=91) 
-                    cha += 32; 
-
-                if(j==0 && cha>=97 && cha<=121) 
-                    cha -= 32; 
-
-                System.out.println(cha);
-                answer += cha;
-            }
-
-
-              answer += " ";
-
-        }
-
-        answer = answer.substring(0,answer.length()-1);
-
-        return answer;
-    }
-} 
-
-/*
- 	class Solution {
-    public String solution(String s) {
-        String answer = "";
         
         s = s.toLowerCase();
         String[] str = s.split(" ");
@@ -77,4 +41,3 @@ class Solution_S021 {
         return answer;
     }
 }
- */
