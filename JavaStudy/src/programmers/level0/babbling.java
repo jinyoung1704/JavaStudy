d728b75218b8344aac976216b679d4ab83edf66e
jinyoung1704 <jinyoung1704@naver.com>
2023-12-08 15:32:45 +0900

    프로그래머스 0단계 옹알이(1)

--- JavaStudy/src/programmers/level0/babbling.java
+++ JavaStudy/src/programmers/level0/babbling.java
@@ -2,25 +2,23 @@
     문제 설명
     머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다.
     문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+
+    ex) babbling : ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]
+        result : 3
  */
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         String[] word = {"aya","ye","woo","ma"};
 
-        for(String s : babbling){
+        for(String ba : babbling){
             for(String w : word){
-                if(s.contains(w)){
-                    System.out.println("s :"+s +" w:"+w);
-                    int index = s.indexOf(w)+w.length();
-                    s = s.substring(index);
-                    System.out.println(s);
+                //System.out.println(ba +","+w);
+                ba = ba.replace(w," ");
 
+                if(ba.replaceAll(" ","").equals("")){
                     answer++;
-                    System.out.println(answer);
-                    if(s.length()==0){
-                        break;
-                    }
+                    break;
                 }
             }
         }
